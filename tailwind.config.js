ebf85f29dc7d5594b4d68d9f798ad4c0898467de
Angel Aguilar <angeljagsal@gmail.com>
2024-06-12 11:09:25 -0500

    Homepage cards and lower navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./www/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'inner-light': 'inset 0 2px 4px rgba(0, 0, 0, 0.05)',  // Más suave
+        'inner': 'inset 0 2px 4px rgba(0, 0, 0, 0.1)',        // Intensidad media
+        'inner-strong': 'inset 0 2px 4px rgba(0, 0, 0, 0.25)' // Más pronunciada
+      }
+    },
   },
   plugins: [],
 }
