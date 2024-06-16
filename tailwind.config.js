5454600868d30e0c43e2a129afd8f05f6878e975
Angel Aguilar <angeljagsal@gmail.com>
2024-06-16 18:49:59 -0500

    User profile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        'inner-light': 'inset 0 2px 4px rgba(0, 0, 0, 0.05)',  // Más suave
-        'inner': 'inset 0 2px 4px rgba(0, 0, 0, 0.1)',        // Intensidad media
-        'inner-strong': 'inset 0 2px 4px rgba(0, 0, 0, 0.25)' // Más pronunciada
+        'inner-light': 'inset 0 2px 4px rgba(0, 0, 0, 0.05)',
+        'inner': 'inset 0 2px 4px rgba(0, 0, 0, 0.1)',
+        'inner-strong': 'inset 0 2px 4px rgba(0, 0, 0, 0.25)'
       }
     },
   },
